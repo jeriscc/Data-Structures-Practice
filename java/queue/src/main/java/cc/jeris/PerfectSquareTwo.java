2f32cc7eee65cd86a436a4913fa64756c7263ab8
jeriscc <jeriscc@users.noreply.github.com>
2019-08-31 00:15:33 -0700

    further optimized DP solution

--- java/queue/src/main/java/cc/jeris/PerfectSquareTwo.java
+++ java/queue/src/main/java/cc/jeris/PerfectSquareTwo.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 /**
  * Dynamic Programming Solution to Perfect Square Problem
  * 
- * Runtime: 23 ms and 60% Memory Usage: 35.3 MB
+ * Runtime: 18 ms and 90% Memory Usage: 35.6 MB
  */
 public class PerfectSquareTwo {
     private int[] sums;
@@ -15,10 +15,16 @@ public class PerfectSquareTwo {
             return 0;
         }
         sums = new int[n + 1];
+        // Filled with 5 because the max num of squares is 4
         Arrays.fill(sums, 5);
         sums[0] = 0;
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j * j <= i; j++) {
+            int sqrt = (int)Math.sqrt(i);
+            if (sqrt*sqrt == i) {
+                sums[i] = 1;
+                continue;
+            }
+            for (int j = 1; j <= sqrt; j++) {
                 sums[i] = Math.min(sums[i], sums[i - j * j] + 1);
             }
         }
